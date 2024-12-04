4e61e9dfce6bf6ffa30d1d6d685052146283aaec
DasonTiovino <114289111+DasonTio@users.noreply.github.com>
2024-12-04 19:13:39 +0700

    Initial Commit

---
+++ MiniChallengeTests/MiniChallengeTests.swift
@@ -0,0 +1,17 @@
+//
+//  MiniChallengeTests.swift
+//  MiniChallengeTests
+//
+//  Created by Dason Tiovino on 04/12/24.
+//
+
+import Testing
+@testable import MiniChallenge
+
+struct MiniChallengeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
